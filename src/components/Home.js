0e9115cd6ae1c60a6e660326340a453620aa32d5
Sai Krishna Veeramreddi <70146573+saikrishnanewdev@users.noreply.github.com>
2022-08-10 22:17:29 -0700

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import "./HomeStyle.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>React Navbar</h1>
+        <h3>Example</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
